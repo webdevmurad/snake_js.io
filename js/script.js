050e38858f7b707aaf106cda54a6edde3d85a3c9
Murad <murad-arslangiraev-1994@mail.ru>
2019-09-07 17:57:51 +0300

    змея движется во все направления , и ест мышей

--- js/script.js
+++ js/script.js
@@ -65,6 +65,8 @@ function createMouse() {
 
 createMouse();
 
+let direction = 'right';
+
 
 // Движение
 
@@ -74,12 +76,40 @@ function move() {
     snakeBody[snakeBody.length-1].classList.remove('snakeBody');
     snakeBody.pop();
 
-    if (snakeCoordinates[0] < 10) {
+    if (direction == 'right') {
+        if (snakeCoordinates[0] < 10) {
         snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] + 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
-    } else {
-        snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
+        }
+    } else if (direction == 'left') {
+        if (snakeCoordinates[0] > 1) {
+        snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] - 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "10"][posY = "' + snakeCoordinates[1] + '"]'));
+        }
+    } else if (direction == 'up') {
+        if (snakeCoordinates[1] < 10) {
+        snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1]+1) + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "1"]'));
+        }
+    } else if (direction == 'down') {
+        if (snakeCoordinates[1] > 1) {
+        snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (snakeCoordinates[1]-1) + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "10"]'));
+        }
     }
 
+    
+    if (snakeBody[0].getAttribute('posX') == mouse.getAttribute('posX') && snakeBody[0].getAttribute('posY') == mouse.getAttribute('posY')) {
+        mouse.classList.remove('mouse');
+        let a = snakeBody[snakeBody.length - 1].getAttribute('posX');
+        let b = snakeBody[snakeBody.length - 1].getAttribute('posY');
+        snakeBody.push(document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]'));
+        createMouse();
+    }
 
     snakeBody[0].classList.add('head');
     for (let i=0; i<snakeBody.length; i++) {
@@ -89,3 +119,19 @@ function move() {
 }
 
 let interval = setInterval(move, 300);
+
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 37 && direction!= 'right') {
+        direction = 'left';
+    }
+    else if (e.keyCode == 38 && direction!= 'down') {
+        direction = 'up';
+    }
+    else if (e.keyCode == 39 && direction!= 'left') {
+        direction = 'right';
+    }
+    else if (e.keyCode == 40 && direction!= 'up') {
+        direction = 'down';
+    }
+});
